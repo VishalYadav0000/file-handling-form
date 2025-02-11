963ea81f303a3a122f642f4f2f1e058423ad9173
vishal yadav <vishal7z86yadav@gmail.com>
2025-02-12 01:37:46 +0530

    adding nodemon

--- app.js
+++ app.js
@@ -3,5 +3,5 @@ const Routes = require("./route")
 const server = http.createServer(Routes);
 
 server.listen(3000, () => {
-    console.log("Server listening on port 3000...");
+    console.log("Server listening on port 3000....");
 });
